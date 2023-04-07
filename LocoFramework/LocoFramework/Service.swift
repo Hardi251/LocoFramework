06aff5cc4c0a408e62cf1d745df2ddb7d8e3e5f4
Hardi251 <hardi.patel@volansys.com>
2023-04-07 11:49:45 +0530

    Added Framework

---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by Hardi Patel on 07/04/23.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Did Some Stuff for Demo Purpose..."
+    }
+}
